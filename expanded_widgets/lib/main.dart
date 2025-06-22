f324088186e69f5bfda4d0437db4438c9300ae99
NischayaPoddar8 <nischayapoddar8@gmail.com>
2025-06-22 13:47:17 +0530

    Initial commit of expanded_widgets project

---
+++ expanded_widgets/lib/main.dart
@@ -0,0 +1,55 @@
+
+import 'package:flutter/material.dart';     
+void main() {
+  runApp(MaterialApp(  
+    home: Home()
+  ),
+  ); 
+}
+class Home extends StatelessWidget {      
+  @override  
+  Widget build(BuildContext context) {
+    return Scaffold(  
+      appBar: AppBar(   
+        title: Text("Hi This Is My First App"), 
+        centerTitle: true, 
+        backgroundColor: Colors.red[500], 
+      ),
+      body:Row(   
+        children: <Widget>[   // we have made a class row having a list of widget data type
+        Expanded(
+          child: Image(
+            image: AssetImage('image/img-1.jpeg'),
+          ),
+          flex: 1,
+        ),
+        Expanded(   // expanded expands the widget and takes as much space available horizontally
+          flex: 1,  // ratio in which we want the space to be divided is given by flex
+          child: Container(            // which conatins various objects inside it children is property of class row
+            padding: EdgeInsets.fromLTRB(5, 10, 10, 5),
+            color: Colors.black,
+            child: Text("1"),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(5, 10, 10, 5),
+            color: Colors.red,
+            child: Text("2"),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child:Container(
+            padding: EdgeInsets.fromLTRB(5, 10, 10, 5),
+            color: Colors.amber,
+            child: Text("3"),
+          ),
+        ),
+        ] // children is a list of data type widget
+      ),
+    );
+  }
+} 
+
--- images_app/lib/main.dart
+++ images_app/lib/main.dart
@@ -15,13 +15,13 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[500], 
       ),
       body:Center(
-      child: Image(       // can be done directly by using Image.asset('URL'),
-      image:AssetImage('assets/img-2.jpeg'), // to asssert images from your saved files
+        child: Image(       // can be done directly by using Image.asset('URL'),
+          image:AssetImage('assets/img-2.jpeg'), // to asssert images from your saved files
+        ),
       ),
       /*child: Image(    // Image.network('URL'),
       image: NetworkImage('https://t4.ftcdn.net/jpg/01/81/33/45/360_F_181334507_GXOokyf7hnWCXsk6UXY8nkUiLAkuj05p.jpg'),
-       used to get images from net      */   
-      ), 
+      used to get images from net      */   
       floatingActionButton: FloatingActionButton(
         onPressed:(){
           print("Button clicked"); 
