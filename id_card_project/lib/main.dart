72e7600dac0c9ec217c7e595088a1ca915466d51
NischayaPoddar8 <nischayapoddar8@gmail.com>
2025-06-22 19:05:10 +0530

    Made pokemon trainer id card

--- id_card_project/lib/main.dart
+++ id_card_project/lib/main.dart
@@ -23,10 +23,24 @@ class TrainerCard extends StatelessWidget{
 
       body: 
       Padding(
-      padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+      padding: EdgeInsets.fromLTRB(30, 40, 30, 0), // spacing from borders 
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: 
         <Widget>[
+        Center(
+          child: CircleAvatar(
+            backgroundImage: AssetImage('image/PngItem_2236259.png'),
+            radius: 50,
+          ),
+        ),
+        Divider(
+          height: 70,
+          color: Colors.grey,
+        ),
+        SizedBox(
+          height: 30,
+        ),
           Text(
             'NAME',
             style: TextStyle(
@@ -34,50 +48,82 @@ class TrainerCard extends StatelessWidget{
               letterSpacing: 2,
             ),
           ),
+          SizedBox(
+          height: 10,   // provides a space of 10 pixels between name and red 
+          ),
           Text(
             'RED',
             style: TextStyle(
-              color: Colors.red,
+              color: Colors.amber,
               letterSpacing: 2,
+              fontSize: 25,  // increases the size of font
+              fontWeight: FontWeight.bold, // bolds the text
             ),
+          ),
+          SizedBox(
+            height: 20,
           ),
           Text(
           'Money',
           style: TextStyle(
-            color: Colors.red,
+            color: Colors.cyan,
             letterSpacing: 2,
           ),
+          ),
+          SizedBox(
+            height: 10,
           ),
             Text(
           '50000',
           style: TextStyle(
-            color: Colors.red,
+            color: Colors.orange,
             letterSpacing: 2,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
+          ),
+          SizedBox(
+            height: 20,
           ),
             Text(
           'Pokedex entries',
           style: TextStyle(
-            color: Colors.red,
+            color: Colors.blue,
             letterSpacing: 2,
           ),
+          ),
+           SizedBox(
+            height: 10,
           ),
             Text(
           '23/500',
           style: TextStyle(
-            color: Colors.red,
-            letterSpacing: 2,
+            color: Colors.green,
+            fontSize: 20,
+            letterSpacing: 2, // spaces within letters
           ),
           ),
-          Image(
-            image: 
-            AssetImage(
-              'image/PngItem_2236259.png',
-            ),
+          SizedBox(
+            height: 20,
           ),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.email,
+                color: Colors.red,
+              ),
+              SizedBox(width: 10,),
+              Text(
+                'nischaya@pokemail.com',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.brown,
+                ),
+              )
+            ],
+          )
         ],
       ),
-      
       ),
     );
   }
